8379e55ace402d8d2a23e590913c74fdb253a8ad
guen9310 <56007272+guen9310@users.noreply.github.com>
2020-04-07 01:24:34 +0900

    test upload

---
+++ src/test/java/com/jade/sbp/controller/HelloControllerTest.java
@@ -0,0 +1,23 @@
+package com.jade.sbp.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+	@Autowired
+	MockMvc mock;
+
+	@Test
+	public void testHello() throws Exception {
+	}
+}
